ace0e0105e6e5adc99e081c5deb4d150111d3a74
youngzhenhao <2499342961@qq.com>
2024-06-04 15:42:40 +0800

    update script

--- buildTrade.ps1
+++ buildTrade.ps1
@@ -4,25 +4,40 @@ $currentFolderName = Split-Path -Path $currentPath -Leaf
 $specificString = "trade"
 $configName = "config.yaml"
 $tradeMarketPath = "/root/tradeMarket"
+$binaryName = "Trade"
 if ($currentFolderName -eq $specificString) {
-    Write-Host "build Trade is in progress, please wait..."
-    if (Test-Path ./$tradeMarketPath) {
+    Write-Host ""
+    Write-Host "Build Trade is in progress, please wait..."
+    if (Test-Path $tradeMarketPath) {
         Write-Host "$tradeMarketPath already exists."
     } else {
         Write-Host "$tradeMarketPath does not exist, new folder will be created."
         mkdir $tradeMarketPath
     }
-    go build -o /root/tradeMarket/Trade ./main/main.go
-    if (Test-Path ./$configName) {
-        Write-Host "$fileName already exists."
+    go build -o $tradeMarketPath/$binaryName ./main/main.go
+    if (Test-Path $tradeMarketPath/$configName) {
+        Write-Host "$tradeMarketPath/$configName already exists."
     } else {
-        Write-Host "$fileName does not exist, new example config file will be copied to current path."
-        Copy-Item ./main/config.yaml.example ./$configName
+        Write-Host "$tradeMarketPath/$configName does not exist, new example config file will be copied to that path."
+        Write-Host "Please edit config in $configName before running trade."
+        Copy-Item ./main/config.yaml.example $tradeMarketPath/$configName
     }
     $end_time = Get-Date
     $time_taken = $end_time - $start_time
-    Write-Host "Time cost: $($time_taken.TotalSeconds) seconds."
+    Write-Host "Build Trade finished, time cost: $($time_taken.TotalSeconds) seconds."
+    Write-Host ""
+    Write-Host "Tips:"
+    Write-Host "    1.Use this fllowing command to start trade in daemon:"
+    Write-Host "        nohup $tradeMarketPath/$binaryName >> $tradeMarketPath/nohup.log 2>&1 &"
+    Write-Host ""
+    Write-Host "    2.Use this fllowing command to check Trade is working properly:"
+    Write-Host "        lsof -i -n -P | grep LISTEN"
+    Write-Host ""
+    Write-Host "    3.Use this fllowing command to stop the Trade:"
+    Write-Host "        kill -9 [PID]"
+    Write-Host ""
+    Set-Location $tradeMarketPath
 } else {
-	Write-Output "Wrong current directory, please run script in trade."
+    Write-Output "Wrong current directory, please run script in trade."
     pause
 }
