123173bf7fc9dd074b912e9bbc30e9f4960ed10e
youngzhenhao <2499342961@qq.com>
2024-06-06 11:49:36 +0800

    feat: add logo

--- utils/printLogo.go
+++ utils/printLogo.go
@@ -2,9 +2,29 @@ package utils
 
 import "fmt"
 
-func PrintAsciiLogo() {
+func PrintAsciiLogoAndInfo() {
 	PrintAsciiShadowLogo()
-	fmt.Println(GetTimeSuffixString())
+	PrintVersion()
+	PrintUrl()
+	fmt.Println("")
+}
+
+func PrintVersion() {
+	fmt.Println("   " + GetVersion())
+}
+
+func PrintUrl() {
+	fmt.Println("   " + GetUrl())
+}
+
+func GetUrl() string {
+	return "https://bitlong.gitbook.io/api-doc/fairlaunch/fairlaunch-trade-rest-api"
+}
+
+func GetVersion() string {
+	base := "0.0.1"
+	version := base + "-" + GetTimeSuffixString()
+	return version
 }
 
 func PrintAsciiShadowLogo() {
@@ -15,5 +35,5 @@ func PrintAsciiShadowLogo() {
 	fmt.Println("      ██║   ██╔══██╗██╔══██║██║  ██║██╔══╝  ")
 	fmt.Println("      ██║   ██║  ██║██║  ██║██████╔╝███████╗")
 	fmt.Println("      ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝")
-	fmt.Println("")
+	//fmt.Println("")
 }
