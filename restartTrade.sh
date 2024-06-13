f615c45cbd9d82671cacec55d4de9b810b83291a
youngzhenhao <2499342961@qq.com>
2024-06-13 09:57:28 +0800

    update script

--- restartTrade.sh
+++ restartTrade.sh
@@ -4,13 +4,13 @@ pid=$(pgrep -f $binaryName)
 if [ -z "$pid" ]; then
   echo "No process named '$binaryName' was found, started a new one."
   nohup /root/trade/Trade >> /root/trade/nohup.log 2>&1 &
-  newPid=$(pgrep -f $binaryName)
-  echo "Trade's new PID is $newPid."
+  pid=$(pgrep -f $binaryName)
+  echo "Trade's new PID is $pid."
 else
   kill $pid
   sleep 2
   nohup /root/trade/Trade >> /root/trade/nohup.log 2>&1 &
   echo "process '$binaryName' was restarted."
-  newPid=$(pgrep -f $binaryName)
-  echo "Trade's new PID is $newPid."
+  pid=$(pgrep -f $binaryName)
+  echo "Trade's new PID is $pid."
 fi
