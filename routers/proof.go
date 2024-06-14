32043cb77a440c45b1cbb954d434e6080dce62ce
youngzhenhao <2499342961@qq.com>
2024-06-14 14:12:38 +0800

    update

--- routers/proof.go
+++ routers/proof.go
@@ -3,12 +3,11 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"trade/handlers"
-	"trade/middleware"
 )
 
 func SetupProofRouter(router *gin.Engine) *gin.Engine {
 	proof := router.Group("/proof")
-	proof.Use(middleware.AuthMiddleware())
+	//proof.Use(middleware.AuthMiddleware())
 	{
 		proof.GET("/download/:asset_id/:proof_name", handlers.DownloadProof)
 	}
