7b9db1c2001d086d3cce885df80ce3eb475d7879
youngzhenhao <2499342961@qq.com>
2024-07-05 11:00:32 +0800

    fix bug

--- routers/addrReceive.go
+++ routers/addrReceive.go
@@ -3,10 +3,12 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"trade/handlers"
+	"trade/middleware"
 )
 
 func SetupAddrReceiveRouter(router *gin.Engine) *gin.Engine {
 	addrReceive := router.Group("/addr_receive")
+	addrReceive.Use(middleware.AuthMiddleware())
 	{
 		addrReceive.GET("/get", handlers.GetAddrReceive)
 		addrReceive.GET("/get/origin", handlers.GetAddrReceiveOrigin)
