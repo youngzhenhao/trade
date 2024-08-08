4da43d705a79cdb1ade009fbf6eb9e6db549a1b2
SevenLiquor <1013871560@qq.com>
2024-08-08 10:27:21 +0800

    feat: fix: updat:登录记录放入线程中执行，并增加path记录

--- models/loginRecord.go
+++ models/loginRecord.go
@@ -6,6 +6,8 @@ type LoginRecord struct {
 	gorm.Model
 	UserId            uint   `gorm:"column:user_id;type:bigint unsigned" json:"userId"` // column选项放在同一个gorm标签内
 	RecentIpAddresses string `json:"recent_ip_addresses" gorm:"type:varchar(255)"`
+	Path              string `json:"path" gorm:"type:varchar(128)"`
+	LoginTime         int    `json:"login_time" gorm:"type:bigint"`
 }
 
 func (LoginRecord) TableName() string {
