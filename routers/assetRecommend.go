103dd829e3844f7c0849ad138df7fc27a7fa4378
youngzhenhao <2499342961@qq.com>
2024-08-12 11:13:31 +0800

    update: add router for asset recommend

--- routers/assetRecommend.go
+++ routers/assetRecommend.go
@@ -13,7 +13,8 @@ func SetupAssetRecommendRouter(router *gin.Engine) *gin.Engine {
 	{
 		assetLocalMint.GET("/get/user", handlers.GetAssetRecommendByUserId)
 		assetLocalMint.GET("/get/asset_id/:asset_id", handlers.GetAssetRecommendByAssetId)
-		assetLocalMint.GET("/get/user/asset_id/:asset_id", handlers.GetAssetRecommendByUserIdAndAssetId)
+		assetLocalMint.GET("/get/user/asset_id/:asset_id", handlers.GetUserAssetRecommendByAssetId)
+		assetLocalMint.POST("/get/user_id_and_asset_id", handlers.GetAssetRecommendByUserIdAndAssetId)
 		assetLocalMint.POST("/set", handlers.SetAssetRecommend)
 	}
 	authorized := router.Group("/asset_recommend", gin.BasicAuth(gin.Accounts{
