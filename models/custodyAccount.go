e37e8af930dea8c554e2341c36fd5f77794f748b
SevenLiquor <1013871560@qq.com>
2024-09-03 09:40:05 +0800

    fix:为account表增加一个唯一键

--- models/custodyAccount.go
+++ models/custodyAccount.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Account struct {
 	gorm.Model
-	UserId          uint          `gorm:"column:user_id;type:bigint unsigned" json:"userId"` // column选项放在同一个gorm标签内
+	UserId          uint          `gorm:"column:user_id;type:bigint unsigned;uniqueIndex:idx_user_id" json:"userId"` // column选项放在同一个gorm标签内
 	UserName        string        `gorm:"column:user_name;type:varchar(100);index:idx_user_name" json:"userName"`
 	UserAccountCode string        `gorm:"column:user_account_code;type:varchar(100)" json:"userAccountCode"`
 	Status          AccountStatus `gorm:"column:status;type:smallint" json:"status"`
