bcaad391388be33647eb123ecc003b03ae1b8d24
youngzhenhao <2499342961@qq.com>
2024-09-26 16:47:51 +0800

    update: GetGroupFirstAssetId

--- handlers/assetGroup.go
+++ handlers/assetGroup.go
@@ -39,6 +39,36 @@ func GetGroupFirstAssetMeta(c *gin.Context) {
 	})
 }
 
+func GetGroupFirstAssetId(c *gin.Context) {
+	tweakedGroupKey := c.Param("group_key")
+	if tweakedGroupKey == "" && len(tweakedGroupKey) != 66 || !utils.IsHexString(tweakedGroupKey) {
+		err := errors.New("invalid tweaked group key")
+		c.JSON(http.StatusOK, models.JsonResult{
+			Success: true,
+			Error:   err.Error(),
+			Code:    models.InvalidTweakedGroupKeyErr,
+			Data:    nil,
+		})
+		return
+	}
+	assetGroup, err := services.GetAssetGroup(tweakedGroupKey)
+	if err != nil {
+		c.JSON(http.StatusOK, models.JsonResult{
+			Success: false,
+			Error:   err.Error(),
+			Code:    models.GetAssetGroupErr,
+			Data:    nil,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, models.JsonResult{
+		Success: true,
+		Error:   models.SuccessErr,
+		Code:    models.SUCCESS,
+		Data:    assetGroup.FirstAssetId,
+	})
+}
+
 func SetGroupFirstAssetMeta(c *gin.Context) {
 	username := c.MustGet("username").(string)
 	userId, err := services.NameToId(username)
--- routers/assetGroup.go
+++ routers/assetGroup.go
@@ -11,6 +11,7 @@ func SetupAssetGroupRouter(router *gin.Engine) *gin.Engine {
 	assetGroup.Use(middleware.AuthMiddleware())
 	{
 		assetGroup.GET("/get/first_meta/group_key/:group_key", handlers.GetGroupFirstAssetMeta)
+		assetGroup.GET("/get/first_asset_id/group_key/:group_key", handlers.GetGroupFirstAssetId)
 		assetGroup.POST("/set/first_meta/", handlers.SetGroupFirstAssetMeta)
 	}
 	return router
