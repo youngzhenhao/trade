72d7ee3f7d9ecc802b0bab821c51ab501db1e5e2
youngzhenhao <2499342961@qq.com>
2024-10-09 16:40:33 +0800

    update: add crud for NftPresale

--- services/nftPresale.go
+++ services/nftPresale.go
@@ -1,3 +1,46 @@
 package services
 
-// TODO
+import (
+	"trade/models"
+	"trade/services/btldb"
+)
+
+func CreateNftPresale(nftPresale *models.NftPresale) error {
+	return btldb.CreateNftPresale(nftPresale)
+}
+
+func CreateNftPresales(nftPresales *[]models.NftPresale) error {
+	return btldb.CreateNftPresales(nftPresales)
+}
+
+func ReadNftPresale(id uint) (*models.NftPresale, error) {
+	return btldb.ReadNftPresale(id)
+}
+
+func ReadAllNftPresales() (*[]models.NftPresale, error) {
+	return btldb.ReadAllNftPresales()
+}
+
+func ReadNftPresalesByNftPresaleState(nftPresaleState models.NftPresaleState) (*[]models.NftPresale, error) {
+	return btldb.ReadNftPresalesByNftPresaleState(nftPresaleState)
+}
+
+func ReadNftPresalesBetweenNftPresaleState(stateStart models.NftPresaleState, stateEnd models.NftPresaleState) (*[]models.NftPresale, error) {
+	return btldb.ReadNftPresalesBetweenNftPresaleState(stateStart, stateEnd)
+}
+
+func ReadNftPresalesByBuyerUserId(userId int) (*[]models.NftPresale, error) {
+	return btldb.ReadNftPresalesByBuyerUserId(userId)
+}
+
+func UpdateNftPresale(nftPresale *models.NftPresale) error {
+	return btldb.UpdateNftPresale(nftPresale)
+}
+
+func UpdateNftPresales(nftPresales *[]models.NftPresale) error {
+	return btldb.UpdateNftPresales(nftPresales)
+}
+
+func DeleteNftPresale(id uint) error {
+	return btldb.DeleteNftPresale(id)
+}
