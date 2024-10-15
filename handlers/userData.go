d076bd413b4f8492185a674e6ac58d05107bb49c
youngzhenhao <2499342961@qq.com>
2024-10-15 16:01:35 +0800

    update: GetUserData router

--- handlers/userData.go
+++ handlers/userData.go
@@ -3,36 +3,42 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"trade/btlLog"
 	"trade/models"
 	"trade/services"
 )
 
 func GetUserData(c *gin.Context) {
 	username := c.Query("username")
-	userData, err := services.GetUserData(username)
+	yaml := c.Query("yaml")
+	isYaml, err := strconv.ParseBool(yaml)
 	if err != nil {
-		c.JSON(http.StatusOK, models.JsonResult{
-			Success: false,
-			Error:   err.Error(),
-			Code:    models.GetUserDataErr,
-			Data:    nil,
-		})
-		return
+		btlLog.UserData.Error("ParseBool err:%v", err)
 	}
-	c.JSON(http.StatusOK, userData)
-}
-
-func GetUserDataYaml(c *gin.Context) {
-	username := c.Query("username")
-	userData, err := services.GetUserDataYaml(username)
-	if err != nil {
-		c.JSON(http.StatusOK, models.JsonResult{
-			Success: false,
-			Error:   err.Error(),
-			Code:    models.GetUserDataYamlErr,
-			Data:    nil,
-		})
-		return
+	if isYaml {
+		userData, err := services.GetUserDataYaml(username)
+		if err != nil {
+			c.JSON(http.StatusOK, models.JsonResult{
+				Success: false,
+				Error:   err.Error(),
+				Code:    models.GetUserDataYamlErr,
+				Data:    nil,
+			})
+			return
+		}
+		c.String(http.StatusOK, "%s", userData)
+	} else {
+		userData, err := services.GetUserData(username)
+		if err != nil {
+			c.JSON(http.StatusOK, models.JsonResult{
+				Success: false,
+				Error:   err.Error(),
+				Code:    models.GetUserDataErr,
+				Data:    nil,
+			})
+			return
+		}
+		c.JSON(http.StatusOK, userData)
 	}
-	c.String(http.StatusOK, "%s", userData)
 }
