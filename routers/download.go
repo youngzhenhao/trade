5c26807161af274d53a9aea5e0964433eec48c85
youngzhenhao <2499342961@qq.com>
2024-11-19 16:29:53 +0800

    update: remove auth for /download

--- routers/download.go
+++ routers/download.go
@@ -2,14 +2,11 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
-	"trade/config"
 	"trade/handlers"
 )
 
 func SetupDownloadRouter(router *gin.Engine) *gin.Engine {
-	authorized := router.Group("/download", gin.BasicAuth(gin.Accounts{
-		config.GetLoadConfig().AdminUser.Username: config.GetLoadConfig().AdminUser.Password,
-	}))
-	authorized.GET("/csv", handlers.CsvDownloadCaptcha)
+	download := router.Group("/download")
+	download.GET("/csv", handlers.CsvDownloadCaptcha)
 	return router
 }
